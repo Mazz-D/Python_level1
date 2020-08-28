275e9d8e30e92009d25c62c297e773cd6160c0f2
Emozozo Moshood <aliumoshood0@gmail.com>
2020-08-28 01:13:46 -0700

    finished some exercise

--- Chapter9/Try_it.py
+++ Chapter9/Try_it.py
@@ -1,9 +1,10 @@
-
-#9-1 Restuarant:
+"""
+#9-1 Restuarant and 9-4 Number served:
 class Restaurant():
     def __init__(self, restaurant_name, cuisine_type):
         self.restaurant_name = restaurant_name
         self.cuisine_type = cuisine_type
+        self.number_served = 0
 
     def describe_restaurant(self):
         print(f"{self.restaurant_name}")
@@ -11,31 +12,49 @@ class Restaurant():
     
     def open_restaurant(self):
         print(f"{self.restaurant_name} is open for business")
+
+    def set_number_served(self, customers_number):
+        self.number_served = customers_number
+        print(f"The {self.restaurant_name} restuarant has served {str(restaurant.number_served)} customers")
+
+    def increment_number_served(self, number_served):
+        self.number_served += number_served
+        print(f"The {self.restaurant_name} restuarant has served {str(restaurant.number_served)} more customers")
     
 restaurant = Restaurant("KFC", "chicken")
 print(restaurant.describe_restaurant())
+restaurant.set_number_served(245)
+restaurant.set_number_served(50)
+restaurant.increment_number_served(3)
 
 #9-2 Three restaurant:
 restaurant_2 = Restaurant("Yahuza", "Suya")
 print(restaurant_2.describe_restaurant())
+restaurant_2.customers_served(10
+)
 
 restaurant_3 = Restaurant("Mama put", "eba,rice")
 print(restaurant_3.describe_restaurant())
 
-#9-3 Users:
+"""
+#9-3 Users and 9-5 Login attempts:
 class User():
     def __init__(self, first_name, last_name, age, sex):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
         self.sex = sex
+        self.login_attempt = 0
     
     def name_sumarry(self):
         print("You filled in the following:")
-        print(f"\n{self.first_name} \n{self.last_name} \n{str(self.age)} \n{self.sex}")
+        print(f"{self.first_name}, {self.last_name}, {str(self.age)}, {self.sex}")
 
     def greet_user(self):
-        print(f"Hello {self.first_name} {self.last_name}, welcome")
+        print(f"Hello {self.first_name} {self.last_name}, welcome to youur profile")
+
+    def increment_login_attempt(self):
+        self.login_attempt += 1
 
 user_1 = User("king", "luff", 20, "M")
 print(user_1.name_sumarry())
